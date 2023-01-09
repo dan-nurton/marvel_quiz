5b369435a8ed4e189d67aadf185b74571fa390f5
Damien Peyrat <da.peyrat@gmail.com>
2023-01-09 18:12:40 +0100

    change components structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,3 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>
-        <header>
-            <div className="banner-container">
-                <h1><a href='/'>Marvel Quiz</a></h1>
-            </div>
-        </header>
-    </div>
-  )
-}
+import Header from "./Header"
 
 export default Header
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,59 @@
+import React, { useRef, useEffect, useState, Fragment } from 'react'
+
+const Landing = () => {
+
+  //Ref to "main" node className welcomePage
+  const refWolverine = useRef(null);
+
+  //useState display buttons -> set to false
+  const [btn , setBtn] = useState(false);
+
+  // useEffet with dependency (empty array) -> componentDidMount
+  useEffect (() => {
+    // add className to "main" node (refWolverine)
+    refWolverine.current.classList.add("startingImg");
+    setTimeout(() => {
+      //  remove className after 3s
+      refWolverine.current.classList.remove("startingImg");
+      // set display button to true
+      setBtn(true);
+    }, 500)
+  }, []);
+
+  // set corner image on mouseOver
+  const setImage = (corner) => {
+    corner === 'left' ?  refWolverine.current.classList.add("leftImg") : refWolverine.current.classList.add("rightImg");
+  
+  }
+
+  // remove className on mouseOut
+  const clearImage = () => {
+    if (refWolverine.current.classList.contains("rightImg")) {
+      refWolverine.current.classList.remove("rightImg");
+    } else if (refWolverine.current.classList.contains("leftImg")) {
+      refWolverine.current.classList.remove("leftImg");
+    }
+  }
+
+  // display buttons only if state btn is set to true
+  const displayButton = btn && (
+    <Fragment>
+      <div className="leftBox">
+        <button onMouseOut={clearImage} onMouseOver = {() => setImage('left')} className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button onMouseOut={clearImage} onMouseOver = {() => setImage('right')} className="btn-welcome">Connexion</button>
+      </div>
+    </Fragment>
+  )
+
+  return (
+    /* ref  useRef = refWolverine*/
+    <main ref={refWolverine} className='welcomePage'>
+      {/* display buttons function */}
+      {displayButton}
+    </main>
+  )
+}
+
+export default Landing
