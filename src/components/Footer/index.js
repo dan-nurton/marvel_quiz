c097ed8896eef632007ecc6e67e15d3795cc3127
Damien Peyrat <da.peyrat@gmail.com>
2023-01-09 15:35:12 +0100

    add footer/Header/Landing components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-container">
+            <p>Projet réalisé par Damien Peyrat - 2023</p>
+            <p>Les icônes Wolverine, Iron-man, Spider-man, Batman, sont récupérées sur le site iconFinder.com</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
