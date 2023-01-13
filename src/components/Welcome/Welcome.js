50509706ca6c3432ae9bec0420fcf54e941d5684
Damien Peyrat <da.peyrat@gmail.com>
2023-01-13 11:06:27 +0100

    add session

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../Firebase';
 import Logout from '../Logout';
 import Quiz from '../Quiz';
 
 function Welcome() {
-  return (
-    <div className="quiz-bg">
-      <div className="container">
-        <Logout />
-        <Quiz />
+  const navigate = useNavigate();
+  const [userSession, setUserSession] = useState(null);
+  useEffect(() => {
+    const listener = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserSession(user);
+      } else {
+        navigate('/');
+      }
+    });
+    return listener();
+  }, [navigate]);
+
+  return userSession === null ? (
+    <>
+      <div className="loader" />
+      <p className="loaderText">Loading</p>
+    </>
+  )
+    : (
+      <div className="quiz-bg">
+        <div className="container">
+          <Logout />
+          <Quiz />
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Welcome;
