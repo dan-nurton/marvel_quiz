9559178bc883dd6b86f95d5d2c8ca0290d59f663
Damien Peyrat <da.peyrat@gmail.com>
2023-01-16 12:18:05 +0100

    Quiz front

---
+++ src/components/Levels/Levels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Levels() {
+  return (
+    <div className="levelsContainer">
+      <h2 className="headingLevels">Débutant</h2>
+    </div>
+  );
+}
+
+export default Levels;
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,13 +1,23 @@
 import React, { useContext } from 'react';
 import { UserSessionContext } from '../App/SessionContext';
+import Levels from '../Levels';
+import ProgressBar from '../ProgressBar';
 
 function Quiz() {
-  const { pseudo, email } = useContext(UserSessionContext);
+  const { pseudo } = useContext(UserSessionContext);
 
   return (
     <div className="test">
       {pseudo}
-      {email}
+      <Levels />
+      <ProgressBar />
+      <h2>Notre Question Quiz</h2>
+      <p className="answerOptions">Question 1</p>
+      <p className="answerOptions">Question 2</p>
+      <p className="answerOptions">Question 3</p>
+      <p className="answerOptions">Question 4</p>
+      <button type="submit" className="btnSubmit">Suivante</button>
+
     </div>
   );
 }
